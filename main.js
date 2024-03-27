c714d79184af33e8d87e332b581e02536a065073
Rubaishaaltaf <160398911+Rubaishaaltaf@users.noreply.github.com>
2024-03-27 10:28:18 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+"use strict";
+// Q6:
+// date 10-March-24
+// let whiteSpace = "\n\t Rubaisha Altaf\t\n"; //\n is used for new line and \t is used for whitespace
+// console.log(whiteSpace);
+// let withoutwhiteSpace = whiteSpace.trim();
+// console.log(withoutwhiteSpace);
+
+// Q11:
+
+// let friendName =["Rubaisha","Ayesha", "Fiza","Rubab"]
+// console.log(friendName[0])
+// console.log(friendName[1])
+// console.log(friendName[2])
+// console.log(friendName[3])
+
+// Q12:
+
+let friendName =["Rubaisha","Ayesha", "Fiza","Rubab"]
+let message=["Follow me on intagram"]
+console.log("Hello",friendName[0],message)
+console.log("Hello",friendName[1],message)
+console.log("Hello",friendName[2],message)
+console.log("Hello",friendName[3],message)
---
+++ main.ts
@@ -0,0 +1,6 @@
+// Q6:
+// date 10-March-24
+let whiteSpace="\n\t Rubaisha Altaf\t\n";
+console.log(whiteSpace);
+let withoutwhiteSpace=whiteSpace.trim();
+console.log(withoutwhiteSpace);
